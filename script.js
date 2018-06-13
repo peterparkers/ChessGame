3071ec8f92e1733b100b2dbf8eaf304c5e53a70d
Phurinat Puekkham <phurinatp@inetasia.com>
2018-06-13 18:05:31 +0700

    create Rect class and selectionTile function

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext('2d');
+var tileSize = 75;
 
 canvas.width = 600;
 canvas.height = 600;
 
 drawGameBoard();
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: Math.floor((evt.clientX - rect.left) / 75),
+      y: Math.floor((evt.clientY - rect.top) / 75)
+    };
+}
 function drawGameBoard () {
-    let tileSize = 75;
-    let row, col;
+    let row, col, i = 0, j = 0;
+    let rect1 = [], rect2 = [];
     for (row = 0; row < 8; row++) {
         for (col = 0; col < 8; col++) {
             if (col % 2 == row % 2) {
-                c.beginPath();
-                c.fillStyle = '#EBEBD0';
-                c.fillRect(col * 75, row * 75, tileSize, tileSize);
-                c.closePath();
+                rect1[i] = new DrawReact(col, row, tileSize, '#EBEBD0');
+                rect1[i].draw();
+                i++;
             }
             else {
-                c.beginPath();
-                c.fillStyle = '#779455';
-                c.fillRect(col * 75, row * 75, tileSize, tileSize);
-                c.closePath();
+                rect2[j] = new DrawReact(col, row, tileSize, '#779455');
+                rect2[j].draw();
+                j++;
             }
         }
     }
 }
+function DrawReact (col, row, tileSize, color) {
+    this.col = col;
+    this.row = row;
+    this.tileSize = tileSize;
+    this.color = color;
+
+    this.draw = function () {
+        c.beginPath();
+        c.fillStyle = this.color;
+        c.fillRect(this.col * 75, this.row * 75, this.tileSize, this.tileSize);
+        c.closePath();
+    }
+    this.changeColor = function (newColor) {
+        c.fillStyle = newColor;
+        c.fillRect(this.col * 75, this.row * 75, this.tileSize, this.tileSize);
+    }
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+    console.log(getMousePos(canvas, evt));
+    
+});
